7457c211565663bab68908a8859fb1b4e90cfef1
Zhasulan <zamanjol1994@mail.ru>
2021-09-10 16:02:21 +0600

    10.09.2021 changes

--- src/views/CreateOrder.vue
+++ src/views/CreateOrder.vue
@@ -22,46 +22,192 @@
             </v-text-field>
 
             <div class="kl_form">
-              <v-select
-                  outlined
-                  dense
-                  v-model="kl_type"
-                  :items="klTypes"
-                  label="КЛ"
+              <v-data-table
+                  :headers="headers"
+                  :items="showKlList"
+                  class="elevation-1"
               >
-              </v-select>
+                <template
+                    v-slot:top
+                >
+                  <v-toolbar
+                      flat
+                  >
+                    <v-toolbar-title>
+                      KL List
+                    </v-toolbar-title>
 
-              <v-text-field
-                  outlined
-                  dense
-                  v-model="summa"
-                  label="Сумма"
-              >
-              </v-text-field>
+                    <v-divider
+                        class="mx-6"
+                        inset
+                        vertical
+                    >
+                    </v-divider>
 
-              <v-select
-                  outlined
-                  dense
-                  v-model="currency"
-                  :items="['тенге','доллар','евро']"
-                  label="Валюта"
+                    <v-spacer>
 
-              >
-              </v-select>
-              <v-btn
-                  outlined
-                  dense
-                  class="mr-4"
-                  @click="pushKl"
-                  color="#4fe612"
-              >
-                Добавить
-              </v-btn>
-              <div class="kl_form__content">
-                <v-chip v-for="(kl, i) in klArray" :key="i">
-                  {{  `${kl.kl_type}, ${kl.summa}, ${kl.currency} ` }}
-                </v-chip>
-              </div>
+                    </v-spacer>
+
+                    <v-dialog
+                        class="kl_form"
+                        v-model="dialog"
+                        max-width="500px"
+                    >
+
+                      <template v-slot:activator="{ on, attrs }">
+                        <v-btn
+                            color="primary"
+                            dark
+                            class="mb-2"
+                            v-bind="attrs"
+                            v-on="on"
+                        >
+                          ADD Kl
+                        </v-btn>
+                      </template>
+
+                      <v-card>
+                        <v-card-text>
+                          <v-container>
+                            <v-select
+                                outlined
+                                dense
+                                v-model="kl_type"
+                                :items="klTypes"
+                                label="КЛ"
+                            >
+                            </v-select>
+                            <v-text-field
+                                v-model="summa"
+                                label="Сумма"
+                            ></v-text-field>
+
+                            <v-select
+                                outlined
+                                dense
+                                v-model="currency"
+                                :items="['тенге','доллар','евро']"
+                                label="Валюта"
+                            >
+                            </v-select>
+                          </v-container>
+                        </v-card-text>
+
+                        <v-card-actions>
+                          <v-spacer></v-spacer>
+                          <v-btn
+                              color="blue darken-1"
+                              text
+                              @click="closeAddKl"
+                          >
+                            Cancel
+                          </v-btn>
+
+                          <v-btn
+                              color="blue darken-1"
+                              text
+                              @click="pushKl"
+                          >
+                            Save
+                          </v-btn>
+                        </v-card-actions>
+                      </v-card>
+                    </v-dialog>
+
+                    <v-dialog
+                        class="kl_form"
+                        v-model="dialogDeleteItem"
+                        max-width="500px"
+                    >
+                      <v-card>
+                        <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+                        <v-card-actions>
+                          <v-spacer></v-spacer>
+                          <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+                          <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                          <v-spacer></v-spacer>
+                        </v-card-actions>
+                      </v-card>
+                    </v-dialog>
+
+                    <v-dialog
+                        class="kl_form"
+                        v-model="dialogEdit"
+                        max-width="500px"
+                    >
+                      <v-card>
+                        <v-card-text>
+                          <v-container>
+                            <v-select
+                                outlined
+                                dense
+                                v-model="klTypeEdit"
+                                :items="klTypes"
+                                label="КЛ"
+                            >
+                            </v-select>
+                            <v-text-field
+                                outlined
+                                dense
+                                v-model="summaEdit"
+                                label="Сумма"
+                            ></v-text-field>
+
+                            <v-select
+                                outlined
+                                dense
+                                v-model="currencyEdit"
+                                :items="['тенге','доллар','евро']"
+                                label="Валюта"
+                            >
+                            </v-select>
+                          </v-container>
+                        </v-card-text>
+
+                        <v-card-actions>
+                          <v-spacer></v-spacer>
+                          <v-btn
+                              color="blue darken-1"
+                              text
+                              @click="dialogEdit = false"
+                          >
+                            Cancel
+                          </v-btn>
+
+                          <v-btn
+                              color="blue darken-1"
+                              text
+                              @click="closeEditDialog"
+                          >
+                            Edit
+                          </v-btn>
+                        </v-card-actions>
+                      </v-card>
+
+                    </v-dialog>
+
+                  </v-toolbar>
+                </template>
+
+                <template v-slot:item.actions="{ item }">
+                  <v-icon
+                      small
+                      class="mr-2"
+                      @click="editItem(item)"
+                  >
+                    mdi-pencil
+                  </v-icon>
+                  <v-icon
+                      small
+                      @click="deleteItem(item)"
+                  >
+                    mdi-delete
+                  </v-icon>
+                </template>
+
+
+
+              </v-data-table>
             </div>
 
             <v-text-field
@@ -70,8 +216,10 @@
                 v-model="beneficator"
                 label="Бенефициар"
             >
+              <!--              beneficator-->
             </v-text-field>
 
+            <!--            send button-->
             <v-btn
                 outlined
                 dense
@@ -84,19 +232,46 @@
 
           </v-form>
         </div>
-
       </v-container>
     </v-main>
   </div>
 </template>
 
 <script>
-import axios from "axios";
+// import axios from "axios";
 
 export default {
   name: "CreateOrder",
+  editedIndex:null,
   data(){
     return{
+      headers: [
+        {
+          text: 'kl',
+          value: 'kl_type',
+          sortable: false
+        },
+        {
+          text: 'Summa',
+          value: 'summa',
+          sortable: false
+        },
+        {
+          text: 'Currency',
+          value: 'currency',
+          sortable: false
+        },
+        { text: 'Actions',
+          value: 'actions',
+          sortable: false
+        }
+      ],
+      dialog: false,
+      klTypeEdit: '',
+      summaEdit: '',
+      currencyEdit: '',
+      dialogEdit: false,
+      dialogDeleteItem: false,
       klTypes: [],
       klArray: [],
       kl_type: '',
@@ -120,9 +295,56 @@ export default {
       this.kl_type=''
       this.summa=''
       this.currency=''
+      this.dialog = false
     },
 
-    submit: function (){
+    closeAddKl () {
+      this.kl_type=''
+      this.summa=''
+      this.currency=''
+      this.dialog = false
+    },
+
+    closeDelete(){
+      this.dialogDeleteItem = false
+    },
+
+    editItem (item) {
+      this.editedIndex = item.index
+      this.klTypeEdit = item.kl_type
+      this.summaEdit = item.summa
+      this.currencyEdit = item.currency
+      this.dialogEdit = true
+    },
+
+    closeEditDialog(){
+      console.log(this.klArray[this.editedIndex-1])
+      const index = this.editedIndex - 1
+      let item = {
+        kl_type: this.klTypeEdit,
+        summa: this.summaEdit,
+        currency: this.currencyEdit
+      }
+      this.klArray[index] = {...item}
+      this.dialogEdit = false
+      console.log(this.klArray)
+    },
+
+    deleteItem (item) {
+      this.editedIndex = item.index-1
+      this.dialogDeleteItem = true
+    },
+
+    deleteItemConfirm () {
+      this.klArray.splice(this.editedIndex,1)
+      this.closeDelete()
+    },
+
+    submit:function (){
+      console.log(1)
+    }
+
+    /*submit: function (){
       this.result = {
         bin: this.bin,
         name: this.name,
@@ -135,11 +357,27 @@ export default {
           .then(response => {
             console.log(response)
           })
-    }
+    }*/
 
   },
+  computed: {
+    showKlList() {
+      return this.klArray.map(
+          (items, index) => ({
+            ...items,
+            index: index + 1
+          }))
+    }
+  },
   watch: {
-    bin(newVal){
+    closeEditDialog(val){
+      val || this.closeEditDialog()
+    },
+
+    klArray(newVal){
+      this.klArray = newVal;
+    }
+    /*bin(newVal){
       if (newVal.length === 12) {
         console.log('get')
         const headers = { "Content-Type": "application/json" };
@@ -157,23 +395,25 @@ export default {
             })
 
       }
-    }
+    }*/
+
+
   }
 }
 </script>
 
 <style scoped>
-  .form{
-    width: 400px;
-    margin: 48px auto;
-  }
-  .kl_form{
-    margin-bottom: 24px;
-    border: 1px solid #dddddd;
-    padding: 12px;
+.form{
+  width: 600px;
+  margin: 48px auto;
+}
+.kl_form{
+  margin-bottom: 24px;
+  border: 1px solid #dddddd;
+  padding: 12px;
 
-  }
-  .kl_form__content {
-    margin-top: 12px;
-  }
+}
+.kl_form__content {
+  margin-top: 12px;
+}
 </style>
